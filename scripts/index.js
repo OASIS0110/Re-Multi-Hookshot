37ee54addb133c4d0b51a53af1433fab9324d80d
OASIS <s2422008@stu.musashino-u.ac.jp>
2024-07-19 19:24:37 +0900

    write index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 //@ts-check
 import { world, system } from '@minecraft/server'
 
+if (!world.scoreboard.getObjective('floatTime')) world.scoreboard.addObjective('floatTime', 'float time');
+if (!world.scoreboard.getObjective('noFallingDamage')) world.scoreboard.addObjective('noFallingDamage', 'no falling damage');
+
 // ロードされているディメンション(プレイヤーがいるディメンション)のリストを取得
 /**
  * @author OASIS0110
@@ -12,6 +15,27 @@ function loadingDimension() {
 		loadingDimension.push(player.dimension.id);
 	}
 	return [...new Set(loadingDimension)]
+}
+
+// フックの引き寄せ終了
+/**
+ * 
+ * @param {import ('@minecraft/server').Entity} seat 
+ * @returns 
+ */
+function end_attract(seat) {
+	const rider = seat.getComponent('minecraft:rideable')?.getRiders()[0];
+	// riderがいない場合終了
+	if (rider == {}) return;
+	// player乗車時
+	if (rider.typeId == 'minecraft:player') {
+		system.runTimeout(() => {
+			seat.triggerEvent('instant_kill');
+		},4)
+		system.runTimeout(() => {
+			rider?.applyKnockback(0.0, 0.0, 0.0, 0.4)
+		},6)
+	}
 }
 
 /**
@@ -28,23 +52,54 @@ function startHook(target, seat) {
 	if (rider !== undefined) world.scoreboard.getObjective('floatTime')?.setScore(rider.getRiders()[0], 30);
 }
 
-if (!world.scoreboard.getObjective('floatTime')) world.scoreboard.addObjective('floatTime', 'float time');
-if (!world.scoreboard.getObjective('noFallingDamage')) world.scoreboard.addObjective('noFallingDamage', 'no falling damage');
-
+// 常時実行
 system.runInterval(() => {
-	//目的地到着検知
+	// ロード中のディメンションに対して実行
+	for (const dimension of loadingDimension()) {
+		// bullet particle
+		for (const bullet of world.getDimension(dimension).getEntities({ type: 're_hookshot:bullet' })) {
+			bullet.runCommand('particle minecraft:basic_crit_particle ~ ~ ~');
+		}
+		//目的地到着、終了実行
+		for (const seat of world.getDimension(dimension).getEntities({ type: 're_hookshot:seat' })) {
+			const destination = seat.getDynamicProperty('re_hookshot:destination');
+			const nowLoc = seat.location;
+			const distance = Math.sqrt((destination?.x - nowLoc.x) ** 2 + (destination?.y - nowLoc.y) ** 2 (destination?.z - nowLoc.z) ** 2)
+			if (distance <= 1) end_attract(seat)
+		}
+	}
 })
 
+// hit entity
 world.afterEvents.projectileHitEntity.subscribe(ev => {
 	const hitEntity = ev.getEntityHit().entity;
 	const projectile = ev.projectile;
 	const source = ev.source;
-	if (source !== undefined && projectile.typeId === 're_hookshot:bullet') {
-		hitEntity?.runCommand(`ride @s summon_ride re_hookshot:seat reassign_rides`);
-		//hitEntity?.runCommand(`summon re_hookshot:marker ~ ~ ~`);
-		const seat = hitEntity?.dimension.getEntities({ type: 're_hookshot:seat', location: hitEntity?.location, closest: 1, maxDistance: 1.0 })[0];
-		if (seat == undefined) return;
-		seat?.setDynamicProperty('re_hookshot:target_loc', source?.location);
-		startHook(source?.location, seat);
+	if (source !== undefined && projectile.typeId === 're_hookshot:bullet' && hitEntity !== undefined) {
+		if (hitEntity.hasTag('attractor')) {
+			source?.runCommand(`ride @s summon_ride re_hookshot:seat reassign_rides`);
+			const seat = hitEntity?.dimension.getEntities({ type: 're_hookshot:seat', location: source.location, closest: 1, maxDistance: 1.0 })[0];
+			if (seat == undefined) return;
+			seat.setDynamicProperty("re_hookshot:destination", hitEntity.location)
+			startHook(hitEntity.location, seat);
+		}
+		else {
+			hitEntity?.runCommand(`ride @s summon_ride re_hookshot:seat reassign_rides`);
+			const seat = hitEntity?.dimension.getEntities({ type: 're_hookshot:seat', location: hitEntity.location, closest: 1, maxDistance: 1.0 })[0];
+			if (seat == undefined) return;
+			seat.setDynamicProperty("re_hookshot:destination", source.location)
+			startHook(source.location, seat);
+		}
+	}
+})
+
+// hit block
+world.afterEvents.projectileHitBlock.subscribe(ev => {
+	const block = ev.getBlockHit().block;
+	const projectile = ev.projectile;
+	const source = ev.source;
+	// seatがブロックに当たったら終了
+	if (source?.typeId == 're_hookshot:seat') {
+		end_attract(projectile)
 	}
 })
